fe94cf33ac5d072ebf0d14502e000165a5747ed6
Nishant <nish.best01@gmail.com>
2019-06-02 13:13:57 +0530

    'Trying'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+// import logo from './logo.svg';
+import { Navbar,Nav,NavDropdown, Form,FormControl,Button,Modal } from 'react-bootstrap';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import './Modal';
+class App extends Component{
+  constructor(props,context){
+    super(props,context);
+    this.state={
+        show:false,
+    };
 }
+  handleShow = (e)=>{
+    // e.preventDefault();
+    this.setState({show:true,});
+        }
+        handleClose =(e)=>{
+            // e.preventDefault();
+            this.setState({show:false,});
+        }
+  render(){
+    return(
+      <div>
+<Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+      <Button variant="primary" onClick={this.handleShow}>
+              Launch demo modal
+            </Button>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+<Modal show={this.state.show} onHide={this.handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={this.handleClose}>
+                  Close
+                </Button>
+                <Button variant="primary" onClick={this.handleClose}>
+                  Save Changes
+                </Button>
+              </Modal.Footer>
+            </Modal>
+      </div>
+    )
+  }
+}
+
 
 export default App;
---
+++ src/Modal.js
@@ -0,0 +1,12 @@
+import React,{Component } from 'react';
+import { Modal,Button} from 'react-bootstrap'
+class MyModal extends Component{
+    
+    render() {
+        return (
+          <>    
+          </>
+        );
+      }
+    }
+export default MyModal;
